1364119f4858bb583d8483bbac3a86acbcbb21be
gabe4278 <60591280+gabe4278@users.noreply.github.com>
2025-11-25 12:20:02 -0600

    Update chatParser.js
    rewrite for optimal parsing

--- chatParser.js
+++ chatParser.js
@@ -56,97 +56,72 @@ const formatSymbol = {
     reset: "\u00a7r"
 }
 
-var textMessage = "";
+var cont = false;
 
-var exactContent = "";
-
-function parseContent(extra) {
-    if (typeof extra == "string") exactContent += extra;
+function parseContent(textComponent) {
+    let text = "";
+    if (typeof textComponent == "string") text += textComponent;
     else {
-        if (colors[extra.color]) exactContent += colors[extra.color];
-        else if (extra.color?.startsWith("#")) {
-            let hcolor = Buffer.from(extra.color.replace("#", ""), "hex");
+        if (colors[textComponent.color]) text += colors[textComponent.color];
+        else if (textComponent.color?.startsWith("#")) {
+            let hcolor = Buffer.from(textComponent.color.replace("#", ""), "hex");
             let r = hcolor.readUInt8();
             let g = hcolor.readUInt8(1);
             let b = hcolor.readUInt8(2);
-            exactContent += `\x1b[38;2;${r};${g};${b}m`;
+            text += `\x1b[38;2;${r};${g};${b}m`;
         }
-        if (extra.obfuscated) exactContent += format.obfuscated;
-        if (extra.bold) exactContent += format.bold;
-        if (extra.strikethrough) exactContent += format.strikethrough;
-        if (extra.underlined) exactContent += format.underlined;
-        if (extra.italic) exactContent += format.italic;
-        if (extra.text) exactContent += extra.text;
-        if (extra.translate) {
-            let content = translateContent[extra.translate];
-            for (let i in extra.with) {
-                content.replace("%s", );
-                content.replace(`%${parseInt(i)+1}$s`, );
+        if (textComponent.obfuscated) text += format.obfuscated;
+        if (textComponent.bold) text += format.bold;
+        if (textComponent.strikethrough) text += format.strikethrough;
+        if (textComponent.underlined) text += format.underlined;
+        if (textComponent.italic) text += format.italic;
+        if (textComponent.text) text += textComponent.text;
+        if (textComponent.translate) {
+            let content = translateContent[textComponent.translate];
+            cont = true;
+            for (let i in textComponent.with) {
+                let w = parseContent(textComponent.with[i]);
+                content = content.replace("%s", w);
+                content = content.replace(`%${parseInt(i)+1}$s`, w);
             }
+            text += content;
+            cont = false;
         }
-        if (extra.extra) {
-            for (let i of extra.extra) {
-                parseExtra(i);
-            }
+        if (textComponent.extra) {
+            cont = true;
+            for (let c of textComponent.extra) text += parseContent(c);
+            cont = false;
         }
     }
-    for (let i in colors) textMessage = textMessage.replaceAll(colorsByFormat[i], colors[i]);
-    for (let i in format) textMessage = textMessage.replaceAll(formatSymbol[i], format[i]);
-    exactContent += "\x1b[0m";
+    for (let i in formatSymbol) text = text.replace(formatSymbol[i], format[i]);
+    for (let i in colorsByFormat) text = text.replace(colorsByFormat[i], colors[i]);
+    return text + (!cont && "\x1b[0m" || "");
 }
 
-function parseExtra(extra) {
-    if (typeof extra == "string") textMessage += extra;
+function parseWithoutFormatting(textComponent) {
+    let text = "";
+    if (typeof textComponent == "string") text += textComponent;
     else {
-        if (colors[extra.color]) textMessage += colors[extra.color];
-        else if (extra.color?.startsWith("#")) {
-            let hcolor = Buffer.from(extra.color.replace("#", ""), "hex");
-            let r = hcolor.readUInt8();
-            let g = hcolor.readUInt8(1);
-            let b = hcolor.readUInt8(2);
-            textMessage += `\x1b[38;2;${r};${g};${b}m`;
+        if (textComponent.text) text += textComponent.text;
+        if (textComponent.translate) {
+            let content = translateContent[textComponent.translate];
+            for (let i in textComponent.with) {
+                let w = parseWithoutFormatting(textComponent.with[i]);
+                content = content.replace("%s", w);
+                content = content.replace(`%${parseInt(i)+1}$s`, w);
+            }
+            text += content;
         }
-        if (extra.obfuscated) textMessage += format.obfuscated;
-        if (extra.bold) textMessage += format.bold;
-        if (extra.strikethrough) textMessage += format.strikethrough;
-        if (extra.underlined) textMessage += format.underlined;
-        if (extra.italic) textMessage += format.italic;
-        if (extra.text) textMessage += extra.text;
-        if (extra.extra) {
-            for (let i of extra.extra) {
-                parseExtra(i);
+        if (textComponent.extra) {
+            for (let c of textComponent.extra) {
+                text += parseWithoutFormatting(c);
             }
         }
     }
-    for (let i in colors) textMessage = textMessage.replaceAll(colorsByFormat[i], colors[i]);
-    for (let i in format) textMessage = textMessage.replaceAll(formatSymbol[i], format[i]);
-    textMessage += "\x1b[0m";
-}
-
-function parseWithoutFormatting(extra) {
-    if (typeof extra == "string") textMessage += extra;
-    else {
-        if (extra.text) textMessage += extra.text;
-        if (extra.translate) {
-            let content = translateContent[extra.translate];
-            for (let i in extra.with) {
-                parseContent(extra.with[i]);
-                content = content.replace("%s", exactContent);
-                content = content.replace(`%${parseInt(i)+1}$s`, exactContent);
-            }
-            textMessage = content;
-        }
-        if (extra.extra) {
-            for (let i of extra.extra) {
-                parseWithoutFormatting(i);
-            }
-        }
-    }
+    return text;
 }
 
 module.exports = function(data, formatting = true) {
-    textMessage = "";
-    if (formatting) parseExtra(data);
-    else parseWithoutFormatting(data);
-    return textMessage;
+    if (formatting) return parseContent(data);
+    else return parseWithoutFormatting(data);
 }
